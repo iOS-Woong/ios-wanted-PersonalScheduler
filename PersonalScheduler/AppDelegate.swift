852eb12c4972d33fc3975230dd96bfb83a712b13
woong <workplayhard1@naver.com>
2023-02-08 05:56:20 +0900

    feat: kakaoNativeAppkey를 활용하여 iOS SDK 초기화

--- PersonalScheduler/AppDelegate.swift
+++ PersonalScheduler/AppDelegate.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        KakaoSDK.initSDK(appKey: AppKeys.kakaoNativeAppKey)
         return true
     }
 
---
+++ PersonalScheduler/AppKeys.swift
@@ -0,0 +1,10 @@
+//
+//  AppKeys.swift
+//  PersonalScheduler
+//
+//  Created by 서현웅 on 2023/02/08.
+//
+
+enum AppKeys {
+    static let kakaoNativeAppKey = "30ac3b0e539fe3e5f5e93e6f7c512bce"
+}
